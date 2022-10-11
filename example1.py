e54c0edabf123168805a50979881e4904cf3e0c7
libowen2121 <libowen.ne@gmail.com>
2022-10-11 16:53:13 +0800

    add function to document

---
+++ example1.py
@@ -0,0 +1,12 @@
+def func1(arg1, arg2):
+    """This is a docstring for func1.
+
+    :param arg1: some value
+    :type arg1: _type_
+    :param arg2: another value
+    :type arg2: _type_
+    :return: arg1
+    :rtype: _type_
+    """
+    arg1 = arg2
+    return arg1

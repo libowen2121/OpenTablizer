1470d6cb015c2080f6bf88aee3693d6dc4e3bfea
libowen2121 <libowen.ne@gmail.com>
2022-10-24 20:45:47 +0800

    Fix bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,8 @@ release = '0.0'
 
 import sys
 import os
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
 sys.path.insert(0, os.path.abspath('../..'))
 
 # -- General configuration ---------------------------------------------------
